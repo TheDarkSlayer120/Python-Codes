a444e586f73ec77ac8ae645a76bd3b35cec9d042
mryes <rk2814240@gmail.com>
2025-10-23 00:02:27 +0100

    Add number ordering script
    This script takes two numbers as input and prints them in ascending order, swapping if necessary.

---
+++ NumberOrder.py
@@ -0,0 +1,17 @@
+first_number = int(input("Input the first number: "))
+second_number = int(input("Input the second number: "))
+
+print("")
+
+print("Ascending order")
+
+print("")
+
+if (first_number > second_number):
+    print("Numbers were swapped")
+    print("First number is: ", second_number)
+    print("Second number is: ", first_number)
+
+else:
+    print("First number is: ", first_number)
+    print("Second number is: ", second_number)
